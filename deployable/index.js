0c4c7abd6fb895ff3d1f6020ec03d2fe77074e09
CammyCow <camille.kao@episodesix.com>
2021-09-18 09:40:10 -0500

    it didn't work, returning to original set up

--- deployable/index.js
+++ deployable/index.js
@@ -1,17 +1,3 @@
-  // Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyDgOQIJswEfH56KFvVAdq8KXZstPyZB3kg",
-  authDomain: "hackriceprojecteating.firebaseapp.com",
-  projectId: "hackriceprojecteating",
-  storageBucket: "hackriceprojecteating.appspot.com",
-  messagingSenderId: "371483673471",
-  appId: "1:371483673471:web:169be295bac547daac904a",
-  measurementId: "G-44KBRM0SXN"
-};
-  firebase.initializeApp(firebaseConfig);
-
-
-
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
