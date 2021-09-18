30d06f650e8f9b921b40dc1e7b2fe9908548f03c
AlexiaHyn <sunfloweralexia@hotmail.com>
2021-09-18 16:24:52 +0800

    rice_grabs/

---
+++ rice_grabs/RiceLinkGrabs.py
@@ -0,0 +1,67 @@
+# used Tutor from this website to scrape data from dynamic websites:
+# https://www.notion.so/Recommand-Algorithms-da42b30bef4c4cd5a09db9e912cbce16
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+from collections import defaultdict
+
+# Instantiate an Options object and tell the browser to run in a headless mode
+opts = Options()
+opts.add_argument(" --headless")
+
+# Set the path to the Google Chrome browser
+chrome_driver = os.getcwd() + "\\chromedriver.exe"
+
+# Set the path to the Chrome webdriver
+driver = webdriver.Chrome(options=opts, executable_path=chrome_driver)
+
+# Load the dining menu pages on Rice website
+urls = ['https://dining.rice.edu/baker-college-kitchen/full-week-menu',
+        'https://dining.rice.edu/north-servery/full-week-menu',
+        'https://dining.rice.edu/seibel-servery/full-week-menu',
+        'https://dining.rice.edu/south-servery/full-week-menu',
+        'https://dining.rice.edu/west-servery/full-week-menu']
+
+menu = defaultdict(lambda: defaultdict(lambda:defaultdict()))
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+serveries = ['Baker Kitchen', 'North Servery', 'Seibel Servery', 'South Servery', 'West Servery']
+
+for index in range(5):
+    driver.get(urls[index])
+
+    # Put the source into a variable and create a BS object from it
+    soup_file = driver.page_source
+    soup = BeautifulSoup(soup_file, 'lxml')
+
+    # Extract menu info in the form of seperated days
+    all_days = soup.find_all('div', {'class':'item'})
+    day_menu_raw = []
+    for day in all_days:
+        day_menu_raw.append(day.get_text())
+
+    # Extract menu info in the form of seperated dishes
+    menu_raw = soup.find_all('div', {'class':'mitem'})
+    discrete_menu = []
+    for dish in menu_raw:
+        discrete_menu.append(dish.get_text())
+
+    lunch_days_meals = day_menu_raw[0:7]
+    dinner_days_meals = day_menu_raw[8:15]
+
+    j = 0
+    for i in range(7):
+        lunch_day_meal = lunch_days_meals[i]
+        menu[days[i]][serveries[index]]['lunch'] = []
+        # assume that the dishes for lunch in different serveries in a day are all different
+        while discrete_menu[j] in lunch_day_meal:
+            menu[days[i]][serveries[index]]['lunch'].append(discrete_menu[j])
+            j += 1
+
+    for i in range(7):
+        dinner_day_meal = dinner_days_meals[i]
+        menu[days[i]][serveries[index]]['dinner'] = []
+        # assume that the dishes for dinner in different serveries in a day are all different
+        while j < len(discrete_menu) and discrete_menu[j] in dinner_day_meal:
+            menu[days[i]][serveries[index]]['dinner'].append(discrete_menu[j])
+            j += 1
