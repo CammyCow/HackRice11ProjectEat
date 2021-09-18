196948cc4de975e3ef6c699fdccb3a07c84829a4
CammyCow <camille.kao@episodesix.com>
2021-09-18 14:54:30 -0500

    created some basic display tests and functions

---
+++ deployable/databaseController.js
@@ -0,0 +1,40 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDgOQIJswEfH56KFvVAdq8KXZstPyZB3kg",
+  authDomain: "hackriceprojecteating.firebaseapp.com",
+  databaseURL: "https://hackriceprojecteating-default-rtdb.firebaseio.com",
+  projectId: "hackriceprojecteating",
+  storageBucket: "hackriceprojecteating.appspot.com",
+  messagingSenderId: "371483673471",
+  appId: "1:371483673471:web:169be295bac547daac904a",
+  measurementId: "G-44KBRM0SXN"
+};
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+
+function displayDish(){
+  console.log("running displayDish");
+  var databaseRef = firebase.database().ref("foodItems");
+  console.log("set databaseRef");
+  databaseRef.on('value',(snapshot) => {
+    const data = snapshot.val();
+    dish = data[7];
+    display(dish["foodName"]);
+    display(dish["date"]);
+    display(dish["isLunch"]);
+    display(dish["place"]);
+  });
+    console.log("finished");
+  }
+
+function display(data){
+  const paragraph = document.createElement('p');
+  var node = document.createTextNode(data);
+  paragraph.appendChild(node);
+  document.getElementById('display').appendChild(paragraph);
+}
+
+function displayMenu(){
+
+}
